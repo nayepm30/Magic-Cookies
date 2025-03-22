a3ad1e5c39e722b433033b15432bdb37ae8628e2
IDGS-805-21000412 <nayepm301@gmail.com>
2025-03-22 01:37:02 -0600

    macro sidebar

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def inicio():
     return render_template("iniciooo.html")
 
+@app.route("/ventas")
+def ventas():
+    return render_template("ventas.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
