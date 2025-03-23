6d7677005c8dcb319822d764e0a5f99a49609a81
IDGS-805-21000412 <nayepm301@gmail.com>
2025-03-23 02:45:17 -0600

    CRUD Proveedores

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def ventas():
 def produccion():
     return render_template("produccion/produccion.html")
 
-@app.route("/proveedores")
+@app.route("/CRUDproveedores")
 def proveedores():
-    return render_template("proveedores/proveedores.html")
+    return render_template("proveedores/CRUDproveedores.html")
 
 if __name__ == "__main__":
     app.run(port=7000, debug=True)
